78d968cd9aa48ae2ead69eb1d0d0c53bbb97c6c2
konkath <93soltys@gmail.com>
2016-12-03 16:31:44 +0100

    Writing to binary file + a little cleanUp

--- patcher.py
+++ patcher.py
@@ -25,40 +25,57 @@ def get_byte_array(my_byte):
     return byte_arr[::-1]
 
 
+def get_addresses(elf_file):
+    entry_point = elf_file.header['e_entry']
+
+    for section in elf_file.iter_sections():
+        if not section.is_null():
+            s_va = section.header['sh_addr']
+            s_fa = section.header['sh_offset']
+            s_end = s_va + s_fa
+
+            if s_va < entry_point < s_end:
+                return s_va, s_fa
+    return None
+
+
+def write_to_file(file, fa, my_byte_array):
+    file.seek(fa)
+    file_bytes = file.read(len(my_byte_array))
+
+    if len(file_bytes) != len(my_byte_array):
+        print('EOF reached - stopping!')
+        raise EOFError
+
+    file.seek(fa)
+    file.write(my_byte_array)
+
+
 def process_file(filename):
-    with open(filename, 'r+b') as f:
-        elf_file = ELFFile(f)
+    with open(filename, 'r+b') as file:
+        elf_file = ELFFile(file)
 
         if not elf_file.little_endian:
             print('little endian is currently not supported')
             return
 
-        entry_point = elf_file.header['e_entry']
-        my_section = None
-        s_va = None
-        s_fa = None
-
-        for section in elf_file.iter_sections():
-            if not section.is_null():
-                s_va = section.header['sh_addr']
-                s_fa = section.header['sh_offset']
-                s_end = s_va + s_fa
-
-                if s_va < entry_point < s_end:
-                    my_section = section
-                    break
-
-        if my_section:
+        s_va, s_fa = get_addresses(elf_file)
+        if s_va and s_fa:
             va = int(sys.argv[2], 16)
             fa = s_fa + (va - s_va)
 
             for my_byte in sys.argv[3:]:
                 try:
-                    get_byte_array(my_byte)
                     my_byte_array = get_byte_array(my_byte)
-                    print(my_byte_array)
-                except AttributeError:
+                    write_to_file(file, fa, my_byte_array)
+                    fa += len(my_byte_array)
+                except AttributeError or EOFError:
                     return
+
+            print('Swapping operation completed with success!')
+        else:
+            print('Invalid section!')
+
 
 if __name__ == '__main__':
     process_file(sys.argv[1])
